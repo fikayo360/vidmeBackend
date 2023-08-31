f7ccfce55d9066a748bd7af65ba996bb280b8d1c
fikayo <fikayoadele@gmail.com>
2023-08-31 20:30:55 +0100

    auth final

--- dist/api/controllers/videoController.js
+++ dist/api/controllers/videoController.js
@@ -14,16 +14,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const http_status_codes_1 = require("http-status-codes");
 const axios_1 = __importDefault(require("axios"));
-const baseUrl = "https://www.googleapis.com/youtube/v3";
 const apikey = process.env.YOUTUBE_KEY;
 class video {
     getVideos(req, res) {
         return __awaiter(this, void 0, void 0, function* () {
             console.log(apikey);
-            const query = `${baseUrl}/search?q=funny&maxResults=20&key=${apikey}&type=video&part=snippet`;
+            const query = 'https://www.googleapis.com/youtube/v3/search?q=funny&regionCode=NG&maxResults=20&key=AIzaSyCW7U3xPDBQMU6mzuAjdrLlsEfaivESoiw&type=video&part=snippet';
             try {
                 const response = yield axios_1.default.get(query);
-                res.status(http_status_codes_1.StatusCodes.OK).json(response);
+                console.log(response);
+                return res.status(http_status_codes_1.StatusCodes.OK).json(response);
             }
             catch (err) {
                 console.log(err.response.data);
--- src/api/controllers/videoController.ts
+++ src/api/controllers/videoController.ts
@@ -3,16 +3,16 @@ import { StatusCodes } from 'http-status-codes';
 import { Request, Response } from 'express';
 import axios from 'axios';
 
-const baseUrl = "https://www.googleapis.com/youtube/v3"
 const apikey = process.env.YOUTUBE_KEY
 
 class video {
     public async getVideos(req: Request, res: Response){
         console.log(apikey);
-        const query = `${baseUrl}/search?q=funny&maxResults=20&key=${apikey}&type=video&part=snippet`
+        const query = 'https://www.googleapis.com/youtube/v3/search?q=funny&regionCode=NG&maxResults=20&key=AIzaSyCW7U3xPDBQMU6mzuAjdrLlsEfaivESoiw&type=video&part=snippet'
         try{
              const response = await axios.get(query);
-              res.status(StatusCodes.OK).json(response)
+             console.log(response);
+              return res.status(StatusCodes.OK).json(response)
         }catch(err:any){
             console.log(err.response.data);
         }
