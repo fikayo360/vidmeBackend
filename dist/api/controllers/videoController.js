dd16e4217ab350b1bc4ece7956d9429bb14c77ef
fikayo <fikayoadele@gmail.com>
2023-09-01 04:22:44 +0100

    auth final

--- dist/api/controllers/videoController.js
+++ dist/api/controllers/videoController.js
@@ -22,7 +22,7 @@ class video {
             let query = 'https://www.googleapis.com/youtube/v3/search?q=funny&regionCode=NG&maxResults=2&key=AIzaSyCW7U3xPDBQMU6mzuAjdrLlsEfaivESoiw&type=video&part=snippet';
             try {
                 const getToken = yield Reset.findAll();
-                console.log(getToken.dataValues);
+                console.log(getToken);
                 // if(getToken){
                 //     query = 'https://www.googleapis.com/youtube/v3/search?q=funny&regionCode=NG&maxResults=2&key=AIzaSyCW7U3xPDBQMU6mzuAjdrLlsEfaivESoiw&type=video&part=snippet'
                 // }
