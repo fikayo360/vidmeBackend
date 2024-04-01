c19f9b2de8609d83eca059e3cd7f2ea4af33c41a
fikayo <fikayoadele@gmail.com>
2024-04-01 15:34:50 +0100

    final

--- src/AppError.tsx
+++ src/AppError.tsx
@@ -1,15 +1,13 @@
 interface AppError {
-    errorCode: number;
     message: string;
     statusCode: number;
 }
 
 class AppError extends Error implements AppError{
-    constructor(errorCode:number,message:string,statusCode:number){
+    constructor(message:string,statusCode:number){
         super(message);
-        this.errorCode = errorCode;
         this.statusCode = statusCode;
     }
 }
 
-module.exports = AppError;
+export default AppError;
